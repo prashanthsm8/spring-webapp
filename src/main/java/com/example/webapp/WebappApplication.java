495e9ba671c2c9aa61ad67e2839039f1793c05b4
prashanthsm8 <prashanthsm8@gmail.com>
2025-12-17 17:36:49 +0530

    commit and push

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -2,9 +2,10 @@ package com.example.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
